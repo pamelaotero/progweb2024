fb5fed9e50df28018580141fadfcdd0a9420771b
Pamela Otéro <pamelasilvaoteroo@gmail.com>
2024-07-10 21:30:58 -0400

    add ts1 exercicio

--- TS1/TS1.js
+++ TS1/TS1.js
@@ -1,1 +1,25 @@
-console.log("eu sou ts1");
+function getCircleArea(ray) {
+  return Math.PI * Math.pow(ray, 2);
+}
+function getCircumference(ray) {
+  return 2 * Math.PI * ray;
+}
+function getRay() {
+  var rayValue = document.forms["myForm"]["ray"].value;
+  return parseFloat(rayValue);
+}
+function setAreaInInput(area) {
+  document.forms["myForm"]["circleArea"].value = area.toFixed(2);
+}
+function setCircumference(circumference) {
+  document.forms["myForm"]["circumference"].value = circumference.toFixed(2);
+}
+function calculateProsByCircle(event) {
+  event.preventDefault(); // Impede o envio do formulário
+  var ray = getRay();
+  var area = getCircleArea(ray);
+  var circumference = getCircumference(ray);
+  setAreaInInput(area);
+  setCircumference(circumference);
+}
+document.forms["myForm"].addEventListener("submit", calculateProsByCircle);
--- TS1/TS1.ts
+++ TS1/TS1.ts
@@ -1,1 +1,42 @@
-console.log("eu sou ts1");
+function getCircleArea(ray: number): number {
+  return Math.PI * Math.pow(ray, 2);
+}
+
+function getCircumference(ray: number): number {
+  return 2 * Math.PI * ray;
+}
+
+function getRay(): number {
+  const form = document.forms.namedItem("myForm") as HTMLFormElement;
+  const rayInput = form.elements.namedItem("ray") as HTMLInputElement;
+  return parseFloat(rayInput.value);
+}
+
+function setAreaInInput(area: number): void {
+  const form = document.forms.namedItem("myForm") as HTMLFormElement;
+  const areaInput = form.elements.namedItem("circleArea") as HTMLInputElement;
+  areaInput.value = area.toFixed(2);
+}
+
+function setCircumference(circumference: number): void {
+  const form = document.forms.namedItem("myForm") as HTMLFormElement;
+  const circumferenceInput = form.elements.namedItem(
+    "circumference"
+  ) as HTMLInputElement;
+  circumferenceInput.value = circumference.toFixed(2);
+}
+
+function calculateProsByCircle(event: Event): void {
+  event.preventDefault();
+  const ray = getRay();
+  const area = getCircleArea(ray);
+  const circumference = getCircumference(ray);
+
+  setAreaInInput(area);
+  setCircumference(circumference);
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  const form = document.forms.namedItem("myForm") as HTMLFormElement;
+  form.addEventListener("submit", calculateProsByCircle);
+});
